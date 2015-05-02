ab3dc2385c67858952cfcbd11f4f9aab2d9825ab
nruest <ruestn@gmail.com>
2015-05-02 13:26:14 -0400

    May 2015 accession

---
+++ 20150501/data/js/payload_details.js
@@ -0,0 +1,5 @@
+var payload_details =  {
+  "tweets" : 2537,
+  "created_at" : "2015-05-02 00:23:38 +0000",
+  "lang" : "en"
+}
---
+++ 20150501/data/js/tweets/2015_05.js
@@ -0,0 +1,353 @@
+Grailbird.data.tweets_2015_05 = 
+[ {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 113, 135 ],
+      "url" : "http:\/\/t.co\/OO4kcRRgPy",
+      "expanded_url" : "http:\/\/fr.wikipedia.org\/w\/index.php?diff=114655401&oldid=105164833&rcid=141057453",
+      "display_url" : "fr.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594270352441741313",
+  "text" : "Saison cyclonique 2015 dans l'oc\u00E9an Atlantique Nord Wikipedia article edited anonymously from Environment Canada http:\/\/t.co\/OO4kcRRgPy",
+  "id" : 594270352441741313,
+  "created_at" : "2015-05-01 22:41:07 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 100, 122 ],
+      "url" : "http:\/\/t.co\/e311OUJMfU",
+      "expanded_url" : "http:\/\/commons.wikimedia.org\/w\/index.php?diff=159379127&oldid=150358941&rcid=164370695",
+      "display_url" : "commons.wikimedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594244783801036800",
+  "text" : "File:Halo solar en Tabasco, M\u00E9xico.JPG Wikipedia article edited anonymously from Environment Canada http:\/\/t.co\/e311OUJMfU",
+  "id" : 594244783801036800,
+  "created_at" : "2015-05-01 20:59:31 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 78, 100 ],
+      "url" : "http:\/\/t.co\/Wv3tOw9FxE",
+      "expanded_url" : "http:\/\/commons.wikimedia.org\/w\/index.php?diff=159378979&oldid=158604604&rcid=164370539",
+      "display_url" : "commons.wikimedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594244400013770752",
+  "text" : "File:Eclissi.JPG Wikipedia article edited anonymously from Environment Canada http:\/\/t.co\/Wv3tOw9FxE",
+  "id" : 594244400013770752,
+  "created_at" : "2015-05-01 20:58:00 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 117, 139 ],
+      "url" : "http:\/\/t.co\/bNhshmd2my",
+      "expanded_url" : "http:\/\/commons.wikimedia.org\/w\/index.php?diff=159378664&oldid=159347034&rcid=164370116",
+      "display_url" : "commons.wikimedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594243695702061056",
+  "text" : "File:Clouds and white sun mixture near AJK,Pakistan.jpg Wikipedia article edited anonymously from Environment Canada http:\/\/t.co\/bNhshmd2my",
+  "id" : 594243695702061056,
+  "created_at" : "2015-05-01 20:55:12 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 109, 131 ],
+      "url" : "http:\/\/t.co\/2F93Gn7qLq",
+      "expanded_url" : "http:\/\/commons.wikimedia.org\/w\/index.php?diff=159378019&oldid=159347023&rcid=164369427",
+      "display_url" : "commons.wikimedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594242175833092099",
+  "text" : "File:20130713Windbruch Schwetzinger Hardt10.jpg Wikipedia article edited anonymously from Environment Canada http:\/\/t.co\/2F93Gn7qLq",
+  "id" : 594242175833092099,
+  "created_at" : "2015-05-01 20:49:09 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 118, 140 ],
+      "url" : "http:\/\/t.co\/SCXBIIrSy2",
+      "expanded_url" : "http:\/\/commons.wikimedia.org\/w\/index.php?diff=159377913&oldid=158626460&rcid=164369319",
+      "display_url" : "commons.wikimedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594241943670005761",
+  "text" : "File:Yacht with red spinnaker at full sail on blue ocean Wikipedia article edited anonymously from Environment Canada http:\/\/t.co\/SCXBIIrSy2",
+  "id" : 594241943670005761,
+  "created_at" : "2015-05-01 20:48:14 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 91, 113 ],
+      "url" : "http:\/\/t.co\/xyTZSbTaTM",
+      "expanded_url" : "http:\/\/en.wikipedia.org\/w\/index.php?diff=660307715&oldid=660294178",
+      "display_url" : "en.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594235292648919040",
+  "text" : "HMS Urge Wikipedia article edited anonymously from Canadian Department of National Defence http:\/\/t.co\/xyTZSbTaTM",
+  "id" : 594235292648919040,
+  "created_at" : "2015-05-01 20:21:48 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 98, 120 ],
+      "url" : "http:\/\/t.co\/3bM77QvvIm",
+      "expanded_url" : "http:\/\/en.wikipedia.org\/w\/index.php?diff=660287109&oldid=660088814",
+      "display_url" : "en.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594201309022134272",
+  "text" : "National Hockey League rivalries Wikipedia article edited anonymously from Shared Services Canada http:\/\/t.co\/3bM77QvvIm",
+  "id" : 594201309022134272,
+  "created_at" : "2015-05-01 18:06:46 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 81, 103 ],
+      "url" : "http:\/\/t.co\/TggTrxsvmZ",
+      "expanded_url" : "http:\/\/en.wikipedia.org\/w\/index.php?diff=660284078&oldid=636923401",
+      "display_url" : "en.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594197163934121984",
+  "text" : "Maitland Avenue Wikipedia article edited anonymously from Shared Services Canada http:\/\/t.co\/TggTrxsvmZ",
+  "id" : 594197163934121984,
+  "created_at" : "2015-05-01 17:50:18 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 94, 116 ],
+      "url" : "http:\/\/t.co\/nysLdYZ974",
+      "expanded_url" : "http:\/\/en.wikipedia.org\/w\/index.php?diff=660275910&oldid=646635024",
+      "display_url" : "en.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594185051891494912",
+  "text" : "Aristagoras Wikipedia article edited anonymously from Canadian Department of National Defence http:\/\/t.co\/nysLdYZ974",
+  "id" : 594185051891494912,
+  "created_at" : "2015-05-01 17:02:10 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 100, 122 ],
+      "url" : "http:\/\/t.co\/4w5RPuXOfL",
+      "expanded_url" : "http:\/\/en.wikipedia.org\/w\/index.php?diff=660260522&oldid=648509003",
+      "display_url" : "en.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594158345310863360",
+  "text" : "Eastport-Kingsgate Border Crossing Wikipedia article edited anonymously from Shared Services Canada http:\/\/t.co\/4w5RPuXOfL",
+  "id" : 594158345310863360,
+  "created_at" : "2015-05-01 15:16:03 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 83, 105 ],
+      "url" : "http:\/\/t.co\/sRXqMMRPXw",
+      "expanded_url" : "http:\/\/en.wikipedia.org\/w\/index.php?diff=660247830&oldid=660159158",
+      "display_url" : "en.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594137652795985921",
+  "text" : "Dolores del R\u00EDo Wikipedia article edited anonymously from Natural Resources Canada http:\/\/t.co\/sRXqMMRPXw",
+  "id" : 594137652795985921,
+  "created_at" : "2015-05-01 13:53:49 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 83, 105 ],
+      "url" : "http:\/\/t.co\/1LZuR0mlpM",
+      "expanded_url" : "http:\/\/fr.wikipedia.org\/w\/index.php?diff=114635550&oldid=114019640&rcid=141015773",
+      "display_url" : "fr.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "594097613990952960",
+  "text" : "Ouragan Leslie (2012) Wikipedia article edited anonymously from Environment Canada http:\/\/t.co\/1LZuR0mlpM",
+  "id" : 594097613990952960,
+  "created_at" : "2015-05-01 11:14:43 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+} ]
