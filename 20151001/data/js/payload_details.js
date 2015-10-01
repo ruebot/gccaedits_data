c8a322d7ba03afc4ddd28ea062eaf09686908441
nruest <ruestn@gmail.com>
2015-10-01 13:51:22 -0400

    October 2015 accession.

---
+++ 20151001/data/js/payload_details.js
@@ -0,0 +1,5 @@
+var payload_details =  {
+  "tweets" : 3917,
+  "created_at" : "2015-10-01 17:49:20 +0000",
+  "lang" : "en"
+}
---
+++ 20151001/data/js/tweets/2015_10.js
@@ -0,0 +1,29 @@
+Grailbird.data.tweets_2015_10 = 
+[ {
+  "source" : "\u003Ca href=\"https:\/\/twitter.com\/gccaedits\" rel=\"nofollow\"\u003EGC CA Edits\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 108, 131 ],
+      "url" : "https:\/\/t.co\/e1GghCX4Eb",
+      "expanded_url" : "https:\/\/en.wikipedia.org\/w\/index.php?diff=683631653&oldid=674062419",
+      "display_url" : "en.wikipedia.org\/w\/index.php?di\u2026"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "649592072442355712",
+  "text" : "38 Canadian Brigade Group Wikipedia article edited anonymously from Canadian Department of National Defence https:\/\/t.co\/e1GghCX4Eb",
+  "id" : 649592072442355712,
+  "created_at" : "2015-10-01 14:29:53 +0000",
+  "user" : {
+    "name" : "Gov. of Canada edits",
+    "screen_name" : "gccaedits",
+    "protected" : false,
+    "id_str" : "2613454796",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/486845019920142336\/D8tyW1L9_normal.png",
+    "id" : 2613454796,
+    "verified" : false
+  }
+} ]
